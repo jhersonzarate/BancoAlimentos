8b25f0d3eb8eacd6b3beb1df4585a35f8339d83e
jherbabuena17 <silvajherson76@gmail.com>
2026-04-04 16:04:29 -0500

    fix cors vercel

--- backend/src/main/java/com/bancoalimentos/backend/config/WebConfig.java
+++ backend/src/main/java/com/bancoalimentos/backend/config/WebConfig.java
@@ -14,9 +14,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+        .allowedOrigins("https://bancoalimentos.vercel.app")
+        .allowedMethods("*")
+        .allowedHeaders("*");
             }
         };
     }
