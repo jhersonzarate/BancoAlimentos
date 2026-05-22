8b38b55d286b5feeda0439287be8376fe1a6712e
jhersonzarate <142950197+jhersonzarate@users.noreply.github.com>
2026-05-22 09:02:26 -0500

    wasasawasa

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://bancoalimentos-production.up.railway.app/api'
+  apiUrl: 'https://bancoalimentos.onrender.com/api'
 };
