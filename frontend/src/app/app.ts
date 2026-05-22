4e1b237b5ffca4c6e86967f799a367cbac6c90e3
jhersonzarate <142950197+jhersonzarate@users.noreply.github.com>
2026-05-22 09:47:42 -0500

    terminado

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,17 @@
 // src/app/app.component.ts
 
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.html'
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(public router: Router) {}
+
+}
